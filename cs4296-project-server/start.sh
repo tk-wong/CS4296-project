a37170577dba43f05d614cd6ea1e509ae32467be
Tkwong <108210425+tk-wong@users.noreply.github.com>
2026-04-21 17:20:14 +0800

    fix bug

--- cs4296-project-server/start.sh
+++ cs4296-project-server/start.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # download and install dependencies
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get install -y curl
+sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get install -y curl
 curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env 
+
 uv sync --no-dev 
 
 # run the application
