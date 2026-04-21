996aab5d8093c7e9d51f54b6f1169c5a4d180580
Tkwong <108210425+tk-wong@users.noreply.github.com>
2026-04-21 17:03:08 +0800

    add start.sh for installing dependencies and start the application

---
+++ cs4296-project-server/start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# download and install dependencies
+sudo apt-get update && && sudo apt-get upgrade && sudo apt-get install -y curl
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv sync --no-dev 
+
+# run the application
+uv run uvicorn main:app --host 0.0.0.0 --port 80
