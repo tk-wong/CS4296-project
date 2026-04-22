9b95c846e80e718b902742755d944232d9e94986
Tkwong <108210425+tk-wong@users.noreply.github.com>
2026-04-22 22:04:23 +0800

    fix docker and EC2 statistics scripts to handle empty data cases

--- cs4296-project-server/ec2-stat.sh
+++ cs4296-project-server/ec2-stat.sh
@@ -8,6 +8,9 @@ while true; do
     # Get CPU and RAM for all processes named 'gunicorn'
     DATA=$(ps -C uvicorn -o %cpu,%mem --no-headers | awk '{cpu+=$1; mem+=$2} END {print cpu "," mem}')
     TIMESTAMP=$(date +%s)
+    if [ -z "$DATA" ]; then
+        continue # Skip if no data is available
+    fi
     echo "$TIMESTAMP,$DATA" >> stats.csv
     sleep 1
 done
