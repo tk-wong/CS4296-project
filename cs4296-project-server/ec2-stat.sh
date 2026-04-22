a4b835b9a6c58b55a7474d61d93219ca78d64d69
Ubuntu <ubuntu@ip-172-31-66-205.ec2.internal>
2026-04-22 14:11:15 +0000

    modify script to handle no data

--- cs4296-project-server/ec2-stat.sh
+++ cs4296-project-server/ec2-stat.sh
@@ -8,7 +8,7 @@ while true; do
     # Get CPU and RAM for all processes named 'gunicorn'
     DATA=$(ps -C uvicorn -o %cpu,%mem --no-headers | awk '{cpu+=$1; mem+=$2} END {print cpu "," mem}')
     TIMESTAMP=$(date +%s)
-    if [ -z "$DATA" ]; then
+    if [ -z "$DATA" || $DATA == ",," ]; then
         continue # Skip if no data is available
     fi
     echo "$TIMESTAMP,$DATA" >> stats.csv
